350c34d0d8525cd772d9e40f8d54100e7893e1fe
Ivan <ivanpakhtusov@gmail.com>
2024-04-12 18:51:01 +0500

    taskitem css

--- client/src/components/TaskItem/TaskItem.jsx
+++ client/src/components/TaskItem/TaskItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./style.css";
 
-function TaskItem({task}) {
+function TaskItem({ task, responsibleUser }) {
   return (
-    <div>
+    <div className="task-item">
       <p>{task.title}</p>
       <p>{task.description}</p>
+      <p>Статус: {task.status}</p>
+      <p>Приоритет: {task.priority}</p>
+      {responsibleUser && <p>Ответственный: {responsibleUser.name}</p>}
     </div>
   );
 }
--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -1,25 +1,47 @@
 import axios from "axios";
+import "./style.css";
 import React, { useEffect, useState } from "react";
 import TaskItem from "../TaskItem/TaskItem";
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
+  const [users, setUsers] = useState({});
+
   useEffect(() => {
     const fetchTasks = async () => {
       try {
         const response = await axios.get("http://localhost:4000/api/tasks");
-        console.log(response);
         setTasks(response.data);
       } catch (error) {
         console.log(error);
       }
     };
+
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/api/users");
+        const usersMap = response.data.reduce((acc, user) => {
+          acc[user.id] = user;
+          return acc;
+        }, {});
+        setUsers(usersMap);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     fetchTasks();
+    fetchUsers();
   }, []);
+
   return (
-    <div>
+    <div className="task-list">
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
+        <TaskItem
+          key={task.id}
+          task={task}
+          responsibleUser={users[task.responsible_id]}
+        />
       ))}
     </div>
   );
