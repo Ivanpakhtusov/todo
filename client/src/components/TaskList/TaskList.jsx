e00b1fc2e0a2ab6711877d2e74a1bbb3a6c0d290
Ivan <ivanpakhtusov@gmail.com>
2024-04-14 22:38:48 +0500

    filter

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -8,6 +8,36 @@ function TaskList({ sessionId, currentUser }) {
   const [tasks, setTasks] = useState([]);
   const [users, setUsers] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  const handleFilterChange = (filter) => {
+    setActiveFilter(filter);
+  };
+
+  const filterTasks = () => {
+    const today = new Date();
+    const oneWeekFromNow = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+    switch (activeFilter) {
+      case "today":
+        return tasks.filter(
+          (task) =>
+            new Date(task.finishedAt).toDateString() === today.toDateString()
+        );
+      case "week":
+        return tasks.filter(
+          (task) =>
+            new Date(task.finishedAt) > today &&
+            new Date(task.finishedAt) <= oneWeekFromNow
+        );
+      case "future":
+        return tasks.filter(
+          (task) => new Date(task.finishedAt) > oneWeekFromNow
+        );
+      default:
+        return tasks;
+    }
+  };
 
   const fetchUsers = async () => {
     try {
@@ -94,6 +124,36 @@ function TaskList({ sessionId, currentUser }) {
           </button>
         )}
       </div>
+      <div className="filter-buttons-container">
+        <button
+          className={`filter-button ${activeFilter === "all" ? "active" : ""}`}
+          onClick={() => handleFilterChange("all")}
+        >
+          All
+        </button>
+        <button
+          className={`filter-button ${
+            activeFilter === "today" ? "active" : ""
+          }`}
+          onClick={() => handleFilterChange("today")}
+        >
+          Today
+        </button>
+        <button
+          className={`filter-button ${activeFilter === "week" ? "active" : ""}`}
+          onClick={() => handleFilterChange("week")}
+        >
+          Week
+        </button>
+        <button
+          className={`filter-button ${
+            activeFilter === "future" ? "active" : ""
+          }`}
+          onClick={() => handleFilterChange("future")}
+        >
+          Future
+        </button>
+      </div>
       <div className="task-list">
         <CreateTaskForm
           open={isModalOpen}
@@ -101,7 +161,7 @@ function TaskList({ sessionId, currentUser }) {
           onCancel={handleCloseModal}
           creatorId={currentUser?.login}
         />
-        {tasks.map((task) => (
+        {filterTasks().map((task) => (
           <div key={task.id}>
             <TaskItem
               task={task}
